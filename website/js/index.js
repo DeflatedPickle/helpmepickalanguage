db1ca4a656b1bf55b3149365963c8a5214f2705b
Dibbo <DeflatedPickle@gmail.com>
2018-02-03 05:22:22 +0000

    Added answers to lists

--- website/js/index.js
+++ website/js/index.js
@@ -3,8 +3,23 @@ var options = [];
 
 function nextQuestion() {
     if (current <= poll.length - 1) {
+        var answers = document.getElementById("answers");
         document.getElementById("question").innerHTML = poll[current].question;
+        removeChildren(answers);
+
+        poll[current].answers.forEach(function (item) {
+            var li = document.createElement("li");
+            answers.appendChild(li);
+
+            li.appendChild(document.createTextNode(item));
+        });
 
         current += 1;
     }
+}
+
+function removeChildren(list) {
+    list.childNodes.forEach(function (item) {
+        list.removeChild(item);
+    });
 }
